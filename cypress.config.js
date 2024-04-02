e88cc68ffdd61719c96775d4033afa08ca208e0a
Rodrigo Lacerda <rodrigo.lacerda@microuniverso.com.br>
2024-04-01 23:29:39 -0300

    Melhoria no codigo , e escrita de mais testes

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
- 
-    "viewportHeight": 880,
-    "viewportWidth": 1280,
+    // "viewportHeight": 880,
+    // "viewportWidth": 1280,
+   
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/CAC.-TAT.cy.js
+++ cypress/e2e/CAC.-TAT.cy.js
@@ -1,12 +1,102 @@
- //<reference types="Cypress" />
+//<reference types="Cypress" />
 
 
 describe('template spec', () => {
-  it('passes', () => {
-    //cy.visit('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
-    cy.visit('./src/index.html')
 
-    cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
-    
+  beforeEach(() => {
+    cy.visit('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+    //cy.visit('./src/index.html')
   })
+
+  it('verificar titulo da aplicação', () => {
+
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+
+  })
+
+  it('preencher os campos obrigatorios e envia o formulario', () => {
+    const textLong = "Teste, teste,, teste,, teste,, teste,, teste,, teste,, teste,, teste,, teste,, teste,, teste,, teste."
+
+    cy.get('#firstName').type('Rodrigo')
+    cy.get('#lastName').type('Lacerda')
+    cy.get('#email').type('rodrigo.lacerda@gmail.com')
+    cy.get('#open-text-area').type(textLong, { delay: 0 })
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+
+    cy.get('#firstName').type('Rodrigo')
+    cy.get('#lastName').type('Lacerda')
+    cy.get('#email').type('rodrigo.lacerda@gmail,com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('valor vazio no campo telefone', () => {
+
+    cy.get('#phone').type('abc').should('have.value', '')
+
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+
+    cy.get('#firstName').type('Rodrigo')
+    cy.get('#lastName').type('Lacerda')
+    cy.get('#email').type('rodrigo.lacerda@gmail.com')
+    cy.get('#phone-checkbox').click()
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+
+  })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+
+    cy.get('#firstName')
+      .type('Rodrigo')
+      .should('have.value', 'Rodrigo')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#lastName')
+      .type('Lacerda')
+      .should('have.value', 'Lacerda')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#email')
+      .type('rodrigo.lacerda@gmail.com')
+      .should('have.value', 'rodrigo.lacerda@gmail.com')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#phone')
+      .type('31984288495')
+      .should('have.value', '31984288495')
+      .clear()
+      .should('have.value', '')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+
+  })
+
+  it.only('envia o formuário com sucesso usando um comando customizado', () => {
+
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+  })
+
 })
