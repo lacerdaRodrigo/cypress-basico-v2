73c6f908722f590c8fa81019788b13ede53a5063
Rodrigo Lacerda <rodrigo.lacerda@microuniverso.com.br>
2024-04-08 23:48:24 -0300

    melhorias

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     // "viewportHeight": 880,
     // "viewportWidth": 1280,
+    video: true,
    
     setupNodeEvents(on, config) {
       // implement node event listeners here
